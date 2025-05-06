9853d92b50deed97852c8b9f5f02e228a2bec470
Michael <123michaelasencio@gmail.com>
2025-05-06 18:01:58 -0400

    Primer commit limpio sin repositorios embebidos

---
+++ Web/Types.hs
@@ -0,0 +1,15 @@
+module Web.Types where
+
+import IHP.Prelude
+import IHP.ModelSupport
+import Generated.Types
+
+data WebApplication = WebApplication deriving (Eq, Show)
+
+
+--data StaticController = WelcomeAction deriving (Eq, Show, Data)
+
+data StaticController 
+    = WelcomeAction
+    | HomeAction -- Añadimos esta línea para nuestra página de inicio
+    deriving (Eq, Show, Data)
