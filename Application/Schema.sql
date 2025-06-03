eeb59c655768fe60ab16d981ee8e87b58208c474
Michael <123michaelasencio@gmail.com>
2025-06-02 20:14:06 -0400

    feat(db): update main database schema
    Update schema to reflect latest migration changes

--- Application/Schema.sql
+++ Application/Schema.sql
@@ -8,3 +8,21 @@ CREATE TABLE processed_maps (
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL
 );
+-- Ejemplo de tabla para la API
+CREATE TABLE users (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL
+);
+
+
+-- Tabla de preguntas para nuestro API
+CREATE TABLE questions (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    question TEXT NOT NULL, 
+    image TEXT,             
+    difficulty TEXT,        
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL
+);
